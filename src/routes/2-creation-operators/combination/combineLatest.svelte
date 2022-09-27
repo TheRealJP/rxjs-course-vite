7877742aacf8e3df408353bca068cc1f5bb7b0c3
Jonathan Peers <jon_athan71@hotmail.com>
2022-09-27 20:00:14 +0200

    add-creation-operators

---
+++ src/routes/2-creation-operators/combination/combineLatest.svelte
@@ -0,0 +1,14 @@
+<script>
+    import Page from "$lib/Page.svelte";
+    import { combineLatest, fromEvent, of, range } from "rxjs";
+
+    const valve$ = fromEvent(document, 'click')
+    const combineLatest$ = combineLatest([]);
+
+
+    range$.subscribe((x) => console.log(x));
+</script>
+
+<section>
+    <Page title="CombineLatest works" subTitle="(Open devtools)" />
+</section>
