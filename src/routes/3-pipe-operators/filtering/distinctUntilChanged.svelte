528bcf53dd54c9663817b061dd7b36fb1ec5dbb0
Jonathan Peers <jon_athan71@hotmail.com>
2023-01-07 21:47:27 +0100

    init state duc

--- src/routes/3-pipe-operators/filtering/distinctUntilChanged.svelte
+++ src/routes/3-pipe-operators/filtering/distinctUntilChanged.svelte
@@ -2,8 +2,6 @@
     import Page from "$lib/Page.svelte";
     import { userMap } from "$utils/constants";
     import type { IUser } from "$utils/interfaces";
-    import { getFullObserver } from "$utils/rxjs-prefab";
-    import { distinctUntilChanged, distinctUntilKeyChanged, from } from "rxjs";
 
     const numbers = [2, 2, 2, 1, 4, 4, 3];
     const users: IUser[] = [
@@ -18,24 +16,8 @@
     ];
 
     /**--- primitives: no arguments needed ---*/
-    from(numbers).pipe(distinctUntilChanged());
-    // .subscribe(getFullObserver("distinctUntilChanged numbers"));
 
     /**--- objects: arguments needed ---*/
-    from(users)
-        .pipe(
-            distinctUntilChanged(),
-            // distinctUntilChanged(
-            //     (previous, current) =>
-            //         previous.profession === current.profession ||
-            //         previous.age === current.age ||
-            //         previous.name === current.name
-            // ), 
-            // distinctUntilChanged((previous, current) => JSON.stringify(previous) === JSON.stringify(current)),
-            // distinctUntilChanged((previous, current) => _.isEqual(previous, current))
-            distinctUntilKeyChanged("profession")
-        )
-        .subscribe(getFullObserver("distinctUntilChanged users"));
 </script>
 
 <section>
