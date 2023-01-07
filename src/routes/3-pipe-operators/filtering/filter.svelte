f9106ce4b2f09d10cc809889460cb9ec37b7fddc
Jonathan Peers <jon_athan71@hotmail.com>
2023-01-07 21:49:45 +0100

    init filter state

--- src/routes/3-pipe-operators/filtering/filter.svelte
+++ src/routes/3-pipe-operators/filtering/filter.svelte
@@ -2,34 +2,9 @@
     import Page from "$lib/Page.svelte";
     import { userMap } from "$utils/constants";
     import type { IUser } from "$utils/interfaces";
-    import { getFullObserver } from "$utils/rxjs-prefab";
-    import { filter, from } from "rxjs";
-
-    const users: IUser[] = [null, userMap.spy, userMap.dad, userMap.baby, userMap.mom];
     
-
-    // basic use case
-    from(users)
-        .pipe(
-            // filter((user) => user.age > 18),
-            // filter((user) => user.profession === "dad" || user.profession === "mom"),
-            // filter((user) => user.name === "John"),
-            // or
-            filter(
-                (user) =>
-                    !!user &&
-                    user.age > 21 &&
-                    (user.profession === "dad" || user.profession === "mom") &&
-                    user.name === "John"
-            ),
-            filter((user) => {
-                throw new Error("Something went wrong");
-            })
-        )
-        .subscribe(getFullObserver("filter"));
-
-    // error use case
-    // from(data).subscribe(getFullObserver("error filter"));
+    const users: IUser[] = [userMap.spy, userMap.dad, userMap.baby, userMap.mom];
+    
 </script>
 
 <section>
