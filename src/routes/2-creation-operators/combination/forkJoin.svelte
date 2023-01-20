36b0904e0df69931eb442500cf4d89fee8bb3854
Jonathan Peers <jon_athan71@hotmail.com>
2023-01-20 18:33:07 +0100

    initial state forkjoin

--- src/routes/2-creation-operators/combination/forkJoin.svelte
+++ src/routes/2-creation-operators/combination/forkJoin.svelte
@@ -1,7 +1,6 @@
 <script lang="ts">
     import Page from "$lib/Page.svelte";
-    import { getFullObserver } from "$utils/rxjs-prefab";
-    import { forkJoin, throwError, timer } from "rxjs";
+    import { throwError, timer } from "rxjs";
     import { fromFetch } from "rxjs/fetch";
 
     let activityOne, activityTwo;
@@ -17,30 +16,6 @@
     // infinite
     const timerInterval1$ = timer(0, 2000);
     const timerInterval2$ = timer(0, 4000);
-
-    // --- forkJoin with finite observables ---
-    forkJoin([activity1$, activity2$]).subscribe({
-        next: ([activity1, activity2]) => {
-            console.log(
-                `I'm going to ${activity1.activity.toLowerCase()} and ${activity2.activity.toLowerCase()}`
-            );
-            activityOne = activity1;
-            activityTwo = activity2;
-        },
-        complete: () => console.log("forkJoin completed!"),
-    });
-
-    // --- forkJoin with infinite observables ---
-    forkJoin([timerInterval1$, timerInterval2$]).subscribe(getFullObserver("infinite"));
-
-    // --- forkJoin with finite and infinite observables ---
-    forkJoin([activity1$, timerInterval1$]).subscribe(getFullObserver("finite and infinite"));
-
-    // --- forkJoin with finite and error observables ---
-    forkJoin([activity1$, error$]).subscribe(getFullObserver("finite & error"));
-
-    // --- forkJoin empty array ---
-    forkJoin([]).subscribe(getFullObserver("empty array"));
 </script>
 
 <section>
