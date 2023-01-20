916e2833e839c96487a1b13ec08d3d02dbda8660
Jonathan Peers <jon_athan71@hotmail.com>
2023-01-20 16:37:07 +0100

    update forkjoin finished

--- src/routes/2-creation-operators/combination/forkJoin.svelte
+++ src/routes/2-creation-operators/combination/forkJoin.svelte
@@ -4,7 +4,8 @@
     import { forkJoin, throwError, timer } from "rxjs";
     import { fromFetch } from "rxjs/fetch";
 
-    // --- observables to combine in the forkJoin operator ---
+    let activityOne, activityTwo;
+    // --- observables to combine with the forkJoin operator ---
     // finite
     const activity1$ = fromFetch("https://www.boredapi.com/api/activity?key=5881028", {
         selector: (response) => response.json(),
@@ -21,33 +22,44 @@
     const forkJoinDefault$ = forkJoin([activity1$, activity2$]);
     // forkJoinDefault$.subscribe((activities) => console.log(activities));
 
-    // destructured array values
-    // forkJoinDefault$.subscribe({
-    //     next: ([activity1, activity2]) => {
-    //         console.log(
-    //             `I'm going to ${activity1.activity?.toLowerCase()} and ${activity2.activity?.toLowerCase()}`
-    //         );
-    //     },
-    //     complete: () => console.log("forkJoin completed!"),
-    // });
+    forkJoin([activity1$, activity2$]).subscribe({
+        next: ([activity1, activity2]) => {
+            console.log(
+                `I'm going to ${activity1.activity.toLowerCase()} and ${activity2.activity.toLowerCase()}`
+            );
+            activityOne = activity1;
+            activityTwo = activity2;
+        },
+        complete: () => console.log("forkJoin completed!"),
+    });
 
     // --- forkJoin with infinite observables ---
-    const forkJoinInfinite$ = forkJoin([timerInterval1$, timerInterval2$]);
-    // forkJoinInfinite$.subscribe(getFullObserver("infinite"));
+    forkJoin([timerInterval1$, timerInterval2$]).subscribe(getFullObserver("infinite"));
 
     // --- forkJoin with finite and infinite observables ---
-    const forkJoinMixed$ = forkJoin([activity1$, timerInterval1$]);
-    // forkJoinMixed$.subscribe(getFullObserver("finite and infinite"));
+    forkJoin([activity1$, timerInterval1$]).subscribe(getFullObserver("finite and infinite"));
 
     // --- forkJoin with finite and error observables ---
-    const forkJoinError$ = forkJoin([activity1$, error$]);
-    forkJoinError$.subscribe(getFullObserver("finite & error"));
+    forkJoin([activity1$, error$]).subscribe(getFullObserver("finite & error"));
 
     // --- forkJoin empty array ---
-    const forkJoinEmpty$ = forkJoin([]);
-    forkJoinEmpty$.subscribe(getFullObserver("empty array"));
+    forkJoin([]).subscribe(getFullObserver("empty array"));
 </script>
 
 <section>
     <Page title="ForkJoin works" subTitle="(Open devtools)" />
+    <section class="border-4 p-4 m-4 flex flex-col justify-center">
+        <h1 class="text-2xl font-bold flex justify-center mb-4 text-gray-400">
+            Combining http requests
+        </h1>
+
+        <div class="mt-2 flex flex-col items-center">
+            <div class="font-bold">Activity One</div>
+            <div>{activityOne?.activity ?? "Loading..."}</div>
+        </div>
+        <div class="mt-4 flex flex-col items-center">
+            <div class="font-bold">Activity Two</div>
+            <div>{activityTwo?.activity ?? "Loading..."}</div>
+        </div>
+    </section>
 </section>
