2fe65c779e6389972faa3f63d0b27c9aa4b66639
Jonathan Peers <jon_athan71@hotmail.com>
2023-02-02 21:08:38 +0100

    update

--- src/routes/2-creation-operators/time/timer-and-interval.svelte
+++ src/routes/2-creation-operators/time/timer-and-interval.svelte
@@ -1,14 +1,15 @@
 <script lang="ts">
     import Page from "$lib/Page.svelte";
     import { interval, Observable, timer } from "rxjs";
+    import { SafeSubscriber } from "rxjs/internal/Subscriber";
 
     /** basic interval operator functionality */
     // interval(2000).subscribe((value) => console.log("interval: ", value));
 
     /** basic timer operator functionality */
-    // timer(2000).subscribe((number) => console.log("timer0: ", number));
-    // timer(2000, 1000).subscribe((number) => console.log("timer1: ", number));
-    // timer(0, 1000).subscribe((number) => console.log("timer2: ", number));
+    // timer(2000).subscribe((value) => console.log("timer: ", value));
+    // timer(2000, 1000).subscribe((value) => console.log("timer: ", value));
+    // timer(0, 1000).subscribe((value) => console.log("timer: ", value));
 
     /** custom timer operator */
     function customTimer(start: number, interval?: number) {
@@ -35,21 +36,18 @@
         });
     }
 
-    // const timerSubscription = customTimer(2000).subscribe((value) =>
-    //     console.log("timer (timeout): ", value)
-    // );
-    // const timerSubscription = customTimer(2000, 1000).subscribe((value) =>
-    //     console.log("timer (timeout + interval): ", value)
-    // );
-    // const timerSubscription = customTimer(0, 1000).subscribe((value) =>
-    //     console.log("timer (interval mimic): ", value)
-    // );
+    // customTimer(2000).subscribe((value) => console.log("timer: ", value));
+    // const timerSubscription = customTimer(2000, 1000).subscribe((value) => console.log("timer: ", value));
+    // const timerSubscription = customTimer(0, 1000).subscribe((value) => console.log("timer: ", value));
 
     // setTimeout(() => {
     //     timerSubscription.unsubscribe();
     // }, 5000);
 
+    // customTimer(5000).subscribe(() => timerSubscription.unsubscribe())
+
     /** custom interval operator */
+
     function customInterval(interval: number) {
         return new Observable<number>((subscriber) => {
             let count = 0;
@@ -65,19 +63,13 @@
         });
     }
 
-    // const intervalSubscription = customInterval(1000).subscribe((value) =>
-    //     console.log(value)
-    // );
-    // setTimeout(() => {
-    //     intervalSubscription.unsubscribe();
-    // }, 3000);
+    // customInterval(2000).subscribe((value) => console.log("interval: ", value));
 
     /** bonus: using specific dates in the timer operator */
     const triggerDate = new Date();
     triggerDate.setSeconds(triggerDate.getSeconds() + 5);
 
-    // timer(triggerDate).subscribe((value) => console.log("dateTimer0: ", value));
-    // timer(triggerDate, 1000).subscribe((value) => console.log("dateTimer0: ", value));
+    // timer(triggerDate, 1000).subscribe((value) => console.log("timer: ", value));
 </script>
 
 <section>
