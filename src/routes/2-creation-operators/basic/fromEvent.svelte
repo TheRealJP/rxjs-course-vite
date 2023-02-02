da588617293adaca61c41a0e2a309a287dc4a00a
Jonathan Peers <jon_athan71@hotmail.com>
2023-02-02 21:50:10 +0100

    wip: fromEvent

--- src/routes/2-creation-operators/basic/fromEvent.svelte
+++ src/routes/2-creation-operators/basic/fromEvent.svelte
@@ -2,57 +2,7 @@
     import { fromEvent, Observable } from "rxjs";
     import { onMount } from "svelte";
 
-    onMount(() => {
-        // const square = document.getElementById("square");
-
-        // const clickEvent$ = fromEvent<MouseEvent>(square, "click");
-        // const clickEventSubscription = clickEvent$.subscribe((event) =>
-        //     console.log("fromEvent: ", {
-        //         eventType: event.type,
-        //         x: event.clientX,
-        //         y: event.clientX,
-        //     })
-        // );
-
-        // const customClickEvent$ = customFromEvent<MouseEvent>(square, "click");
-        // const customClickEventSubscription = customClickEvent$.subscribe(
-        //     (event) => {
-        //         console.log("customFromEvent: ", {
-        //             eventType: event.type,
-        //             x: event.clientX,
-        //             y: event.clientX,
-        //         });
-        //     }
-        // );
-
-        // setTimeout(() => {
-        //     clickEventSubscription.unsubscribe();
-        //     customClickEventSubscription.unsubscribe();
-        //     console.log("Unsubscribed");
-        // }, 3000);
-
-        // -------- other event example, works the same way
-        const input = document.getElementById("input");
-        const keyDownEvent$ = fromEvent<KeyboardEvent>(input, "keydown");
-        keyDownEvent$.subscribe((event) => {
-            console.log("fromEvent keydown: ", {
-                eventType: event.type,
-                key: event.key,
-            });
-        });
-
-        customFromEvent<KeyboardEvent>(input, "keydown").subscribe((event) =>
-            console.log("customFromEvent keydown: ", {
-                eventType: event.type,
-                key: event.key,
-            })
-        );
-    });
-
-    function customFromEvent<T extends Event>(
-        target: HTMLElement | Document,
-        eventName: string
-    ) {
+    function customFromEventt<T extends Event>(target: HTMLElement | Document, eventName: string) {
         return new Observable<T>((subscriber) => {
             const eventHandler = (event: T) => {
                 // console.log("customFromEvent producer: ", event.type); // eventHandler
@@ -66,14 +16,37 @@
             };
         });
     }
+
+    // move on from here
+
+    onMount(() => {
+        const square = document.getElementById("square");
+
+        const clickEvent$ = fromEvent<MouseEvent>(square, "click");
+
+        clickEvent$.subscribe((event) => {
+            console.log("fromEvent: ", { eventTarget: event.type, x: event.x, y: event.y });
+        });
+
+        // -------- other event example, works the same way
+        const input = document.getElementById("input");
+        const keyDownEvent$ = customFromEventt<KeyboardEvent>(input, "keydown").subscribe((event) =>
+            console.log("customFromEvent keydown: ", {
+                eventType: event.type,
+                key: event.key,
+            })
+        );
+
+        // setTimeout(() => {
+        //     clickEventSubscription.unsubscribe();
+        //     customClickEventSubscription.unsubscribe();
+        //     console.log("Unsubscribed");
+        // }, 3000);
+    });
 </script>
 
 <section class="flex flex-col h-60 justify-center items-center">
     <div id="square" class="w-36 h-36 bg-blue-600 m-6" />
 
-    <input
-        id="input"
-        type="text"
-        class="border border-solid border-gray-500 rounded-sm"
-    />
+    <input id="input" type="text" class="border border-solid border-gray-500 rounded-sm" />
 </section>
