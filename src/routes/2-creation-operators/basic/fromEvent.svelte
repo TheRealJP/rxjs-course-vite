bf6732934e1e092cac982e9996d0f9352a708ab5
Jonathan Peers <jon_athan71@hotmail.com>
2023-02-04 16:52:42 +0100

    initial stage fromEvent

--- src/routes/2-creation-operators/basic/fromEvent.svelte
+++ src/routes/2-creation-operators/basic/fromEvent.svelte
@@ -1,43 +1,9 @@
 <script lang="ts">
-    import { fromEvent, Observable } from "rxjs";
     import { onMount } from "svelte";
-
-    function customFromEvent<T extends Event>(
-        eventTarget: Document | HTMLElement,
-        eventName: string
-    ) {
-        return new Observable<T>((subscriber) => {
-            const eventHandler = (event: T) => {
-                subscriber.next(event);
-            };
-
-            eventTarget.addEventListener(eventName, eventHandler);
-
-            return () => {
-                eventTarget.removeEventListener(eventName, eventHandler);
-            };
-        });
-    }
 
     onMount(() => {
         const square = document.getElementById("square");
         const input = document.getElementById("input");
-
-        const clickEvent$ = fromEvent<MouseEvent>(square, "click");
-
-        const clickSubscription = clickEvent$.subscribe((event) =>
-            console.log("fromEvent: ", { eventType: event.type, x: event.x, y: event.y })
-        );
-
-        const keyDownSubscription = customFromEvent<KeyboardEvent>(input, "keydown").subscribe(
-            (event) =>
-                console.log("customFromEvent keydown: ", { eventType: event.type, key: event.key })
-        );
-
-        setTimeout(() => {
-            clickSubscription.unsubscribe();
-            keyDownSubscription.unsubscribe();
-        }, 5000);
     });
 </script>
 
