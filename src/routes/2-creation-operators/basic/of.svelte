5d7000c59c7541d1c092f7ea6de48c0ceed6b9d6
Jonathan Peers <jon_athan71@hotmail.com>
2023-02-07 21:29:09 +0100

    initial state of

--- src/routes/2-creation-operators/basic/of.svelte
+++ src/routes/2-creation-operators/basic/of.svelte
@@ -1,25 +1,5 @@
 <script lang="ts">
     import Page from "$lib/Page.svelte";
-    import { from, Observable, of, Subscriber } from "rxjs";
-
-    const of$ = of(3.14, [1, 2, 3], "abc", Promise.resolve("promiseValue"), () => "hi");
-
-    // of$.subscribe({
-    //     next: (value) => console.log(value),
-    //     complete: () => console.log("of subscription completed"),
-    // });
-
-    function customOf<T>(...args): Observable<T> {
-        return new Observable((subscriber: Subscriber<T>) => {
-            args.forEach((value) => subscriber.next(value));
-            subscriber.complete();
-        });
-    }
-
-    customOf(3.14, [1, 2, 3], "abc", Promise.resolve("promiseValue"), () => "hi").subscribe({
-        next: (value) => console.log(value),
-        complete: () => console.log("customOf subscription completed"),
-    });
 </script>
 
 <section>
