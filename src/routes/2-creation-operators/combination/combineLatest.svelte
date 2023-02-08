002b62263ff6ea8dfdaf12ca8454545691f5989c
Jonathan Peers <jon_athan71@hotmail.com>
2023-02-08 09:27:57 +0100

    update finished states previous lessons

--- src/routes/2-creation-operators/combination/combineLatest.svelte
+++ src/routes/2-creation-operators/combination/combineLatest.svelte
@@ -2,11 +2,9 @@
     import { schoolClassMap } from "$utils/constants";
     import type { ISchoolClass } from "$utils/interfaces";
     import { getFullObserver } from "$utils/rxjs-prefab";
-    import { combineLatest, from, fromEvent, interval, of, range, throwError, timer } from "rxjs";
-    import { ajax } from "rxjs/ajax";
+    import { fromEvent, throwError } from "rxjs";
     import { fromFetch } from "rxjs/fetch";
     import { onMount } from "svelte";
-    const fullObserver = getFullObserver("combineLatest");
 
     /** --- basic behaviors --- */
     const mouseClick$ = fromEvent<MouseEvent>(document, "click");
@@ -15,25 +13,7 @@
         selector: (response) => response.json(),
     });
     const error$ = throwError(() => "Something went wrong");
-
-    const combineLatest$ = combineLatest([httpCall$, mouseClick$, keyInput$]);
-
-    // combineLatest$.subscribe(([httpResponse, mouseClickData, keyInputData]) => {
-    //     console.log({
-    //         httpResponse: httpResponse.activity,
-    //         mousePosition: `${mouseClickData.clientX}/${mouseClickData.clientY}`,
-    //         keyboardKey: keyInputData.key,
-    //     });
-    // });
-
-    // --- combineLatest missing values ---
-    // --- combineLatest finite & infinite ---
-    // --- combineLatest complete ---
-    // --- combineLatest error ---
-    // combineLatest([mouseClick$, error$]).subscribe(fullObserver);
-
-    // --- combineLatest empty array ---
-    // combineLatest([]).subscribe(fullObserver);
+    const fullObserver = getFullObserver("combineLatest");
 
     /** --- classic combineLatest use case: applying filter criteria --- */
     const schoolClasses: ISchoolClass[] = [
@@ -47,34 +27,6 @@
     onMount(() => {
         const categoryDropdown = document.getElementById("category-dropdown");
         const titleSearch = document.getElementById("title-search");
-
-        // --- we'll also do this part together ---
-        const categoryDropdown$ = fromEvent<Event>(categoryDropdown, "change");
-        const titleSearch$ = fromEvent<InputEvent>(titleSearch, "input");
-
-        const subscription = combineLatest([categoryDropdown$, titleSearch$]).subscribe(
-            ([dropdownValue, titleValue]) => {
-                const title = (titleValue.target as HTMLInputElement).value;
-                const category = (dropdownValue.target as HTMLSelectElement).value;
-
-                filteredClasses = schoolClasses.filter(
-                    (cl) => cl.title.includes(title) && cl.category === category
-                );
-
-                // if default is selected show all the classes and only allow filtering on title
-                if (category === "default") {
-                    filteredClasses = schoolClasses.filter((cl) => cl.title.includes(title));
-                }
-            }
-        );
-
-        // fromEvent is an infinite observable, so normally we need to unsubscribe the combineLatest subscription
-        // for this use case we dont unsubscribe because we want to keep filtering
-        // this unsubscribe is just for demo purposes
-        timer(7000).subscribe(() => {
-            subscription.unsubscribe();
-            console.log("combineLatest unsubscribed!");
-        });
     });
 </script>
 
