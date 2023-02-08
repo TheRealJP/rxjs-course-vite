1c24b5a81eb0d4583dd4d624345ec9b05c5c2ea4
Jonathan Peers <jon_athan71@hotmail.com>
2023-02-08 09:51:25 +0100

    update previous lessons to final stage

--- src/routes/2-creation-operators/basic/from.svelte
+++ src/routes/2-creation-operators/basic/from.svelte
@@ -2,36 +2,39 @@
     import Page from "$lib/Page.svelte";
     import { from, of, throwError } from "rxjs";
 
-    // main difference between of & from
-    // of([1, 2, 3], [3, 2, 1]).subscribe((v) => console.log(v));
-    // from([1, 2, 3]).subscribe((v) => console.log(v));
-    
-    // string or other iterable types
-    from("God").subscribe((v) => console.log(v));
-    
-    // not possible to pass primitives like number or boolean
-    // from(3).subscribe((v) => console.log(v));
-    // from(true).subscribe((v) => console.log(v));
+    /** main difference between of & from */
+    // of([1, 2 ,3]).subscribe((value) => console.log("of: ", value));
+    // from([1, 2 ,3]).subscribe((value) => console.log("from: ", value));
 
-    // array or other collection data structure
-    const map = new Map();
-    map.set(1, 'Hello');
-    map.set(2, 'World');
-    from(map).subscribe((v) => console.log(v));
-    
-    // promise
-    from(Promise.reject("promise value")).subscribe({
-        next: (v) => console.log(v),
-        error: (err) => console.error("promise returned error: ", err),
-    });
-    
-    // another observable
-    // of("observable inside from")
-    // throwError(() => 'error')
-    from(throwError(() => "error")).subscribe({
-        next: (v) => console.log(v),
-        error: (err) =>
-            console.error("observable inside from returned error: ", err),
+    /** strings or other iterables */
+    // from("God").subscribe((v) => console.log(v));
+    // from("God").subscribe((value) => console.log("string from: ", value));
+
+    /** arrays or other collection data structures */
+    // const map = new Map();
+    // map.set(1, "Hello");
+    // map.set(2, "World");
+    // from(map).subscribe((value) => console.log("map from: ", value));
+
+    /** promises */
+    // from(Promise.resolve("by the time you read this i already got emitted to an observer through subscriber.next()")).subscribe({
+    //     next: (value) => console.log(value),
+    //     complete: () => console.log("from promise completed!")
+    // })
+
+    // from(Promise.reject("something went wrong!")).subscribe({
+    //     next: (value) => console.log(value),
+    //     error: (error) => console.error(error)
+    // })
+
+    /** observables as an argument */
+    // from(of("of inside from")).subscribe({
+    //     next: (value) => console.log(value),
+    // });
+
+    from(throwError(() => "custom error message")).subscribe({
+        next: (value) => console.log(value),
+        error: (error) => console.error("observable inside from operator threw an error: ", error)
     });
 </script>
 
--- src/routes/2-creation-operators/combination/combineLatest.svelte
+++ src/routes/2-creation-operators/combination/combineLatest.svelte
@@ -1,45 +1,32 @@
 <script lang="ts">
     import { schoolClassMap } from "$utils/constants";
     import type { ISchoolClass } from "$utils/interfaces";
-    import { getFullObserver } from "$utils/rxjs-prefab";
-    import { combineLatest, fromEvent, of, throwError, timer } from "rxjs";
-    import { ajax } from "rxjs/ajax";
+    import { combineLatest, fromEvent, throwError, timer } from "rxjs";
     import { fromFetch } from "rxjs/fetch";
     import { onMount } from "svelte";
-    const fullObserver = getFullObserver("combineLatest");
 
     /** --- basic behaviors --- */
-    // --- observables to combine in the combineLatest operator ---
+    const mouseClick$ = fromEvent<MouseEvent>(document, "click");
+    const keyInput$ = fromEvent<KeyboardEvent>(document, "keydown");
     const httpCall$ = fromFetch("https://www.boredapi.com/api/activity?key=5881028", {
         selector: (response) => response.json(),
     });
-    const mouseClick$ = fromEvent<MouseEvent>(document, "click");
-    const keyInput$ = fromEvent<KeyboardEvent>(document, "keydown");
     const error$ = throwError(() => "Something went wrong");
 
-    // --- more common: using infinite observables (sometimes mixed with finite observables) ---
     const combineLatest$ = combineLatest([httpCall$, mouseClick$, keyInput$]);
-
     // combineLatest$.subscribe(([httpResponse, mouseClickData, keyInputData]) => {
     //     console.log({
     //         httpResponse: httpResponse.activity,
     //         mousePosition: `${mouseClickData.clientX}/${mouseClickData.clientY}`,
     //         keyboardKey: keyInputData.key,
     //     });
-    // });
+    // })
 
-    // --- less common: using finite observables ---
-    const of1$ = of(3);
-    const of2$ = of("C");
-    // combineLatest([of1$, of2$]).subscribe(fullObserver);
-
-    // --- combineLatest error ---
     // combineLatest([mouseClick$, keyInput$, error$]).subscribe(fullObserver);
 
-    // --- combineLatest empty array ---
     // combineLatest([]).subscribe(fullObserver);
 
-    /** --- classic combineLatest use case: applying filter criteria --- */
+    /** --- common use case for combineLatest: applying filter criteria --- */
     const schoolClasses: ISchoolClass[] = [
         schoolClassMap.biology,
         schoolClassMap.mathematics,
@@ -51,8 +38,6 @@
     onMount(() => {
         const categoryDropdown = document.getElementById("category-dropdown");
         const titleSearch = document.getElementById("title-search");
-
-        // --- we'll also do this part together ---
         const categoryDropdown$ = fromEvent<Event>(categoryDropdown, "change");
         const titleSearch$ = fromEvent<InputEvent>(titleSearch, "input");
 
@@ -72,9 +57,6 @@
             }
         );
 
-        // fromEvent is an infinite observable, so normally we need to unsubscribe the combineLatest subscription
-        // for this use case we dont unsubscribe because we want to keep filtering
-        // this unsubscribe is just for demo purposes
         timer(7000).subscribe(() => {
             subscription.unsubscribe();
             console.log("combineLatest unsubscribed!");
