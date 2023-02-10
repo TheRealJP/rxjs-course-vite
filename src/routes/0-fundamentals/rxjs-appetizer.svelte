2f9788103ebff547c4fda29dfa0c1b1b81d0cf55
Jonathan Peers <jon_athan71@hotmail.com>
2023-02-10 12:25:10 +0100

    initial state appetizer

--- src/routes/0-fundamentals/rxjs-appetizer.svelte
+++ src/routes/0-fundamentals/rxjs-appetizer.svelte
@@ -1,39 +1,12 @@
 <script lang="ts">
-	import { activityErrorHttpCall$, activityErrorPromise, activityHttpCall$, activityPromise } from '$utils/rxjs-prefab';
     import Page from "$lib/Page.svelte";
-    import type { IActivity } from "$utils/interfaces";
 
     /** eventListener */
-    document.addEventListener("click", (clickEvent) => {
-        try {
-            console.log(clickEvent.clientX);
-        } catch (error) {
-            console.error(error);
-        }
-    });
-
-    // some variable/state that needs to be updated
-    let currentActivity = "nothing";
-
+  
     /** promise */
-    activityPromise.then(
-        (activity: IActivity) => {
-            currentActivity = activity.description;
-            console.log(currentActivity);
-        },
-        (error) => {
-            console.error(error);
-        }
-    );
 
     /** observable */
-    activityHttpCall$.subscribe({
-        next: (activity: IActivity) => {
-            currentActivity = activity.description;
-            console.log(currentActivity);
-        },
-        error: (error) => console.error(error),
-    });
+
 </script>
 
 <section>
